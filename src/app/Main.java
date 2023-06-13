92ef0621441a1af33f530ca545254f43871e7872
Frolov1987 <116515964+Frolov1987@users.noreply.github.com>
2023-06-13 17:08:31 +0300

    Edit Viev Calculator Text

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,9 @@
 package app;
 
 public class Main {public static void main(String[] args) {
-        System.out.println("Calculator app.");
+        System.out.println("My git app. Super!");
+        System.out.println("Version 1.0");
+
 }
 
 }
