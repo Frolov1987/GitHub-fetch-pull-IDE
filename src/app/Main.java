50fcc499028d5f7239ec5a92692bc08c0bd36d44
Frolov1987 <116515964+Frolov1987@users.noreply.github.com>
2023-06-13 17:21:26 +0300

    Chenge Version App

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,7 @@
 package app;
 
 public class Main {public static void main(String[] args) {
-        System.out.println("Version 1.0");
+        System.out.println("Version 2.0");
         System.out.println("Calculator app.");
         }
 }
